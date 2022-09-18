418685deaf11ff85bbc1640e188eecb83b6d0823
Calvin Anderson <andersoncalvin289@gmail.com>
2022-09-18 09:56:35 -0500

    formatting changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["Software Engineer", "Graduate Student", "Certified Public Accountant"],
+    strings: [" ", "Software Engineer", "Graduate Student", "Certified Public Accountant"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
